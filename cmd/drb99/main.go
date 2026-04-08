6696d372131545618bf70cde22827147bead362a
bashNeko <bashnko@users.noreply.github.com>
2026-04-09 04:52:26 +0530

    feat(cmd): wire up service dependencies

---
+++ cmd/drb99/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	gh "github.com/bashnko/drb99/github"
+	"github.com/bashnko/drb99/handler"
+	service "github.com/bashnko/drb99/services"
+)
+
+func main() {
+	addr := os.Getenv("DRB99")
+	if addr == "" {
+		addr = ":8080"
+	}
+
+	ghClient := gh.NewClient()
+	// genarotor to be done
+	svc := service.New(ghClient, gen)
+	h := handler.New(svc)
+
+	mux := http.NewServeMux()
+	h.Register(mux)
+
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           mux,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+	log.Printf("drb99 listening on %s", addr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
+}

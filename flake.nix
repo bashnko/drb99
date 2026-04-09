9ca5a1ade22d5247c15df92efde718ab12590072
bashNeko <bashnko@users.noreply.github.com>
2026-04-09 15:25:49 +0530

    feat(deploy): setup fly.io and nix environment
    - add Dockerfile and fly.toml to get things running on fly.io
    - setup github action for auto-deploys to main
    - add nix flake and .envrc for a cleaner dev shell

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          name = "dev shell";
+
+          packages = with pkgs; [
+            flyctl
+          ];
+
+          shellHook = ''
+
+          '';
+        };
+      }
+    );
+}
+
